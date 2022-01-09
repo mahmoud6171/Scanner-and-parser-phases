9b4dad985f485c8cb5b388d701ffc0d539026c65
mahmoud6171 <60368309+mahmoud6171@users.noreply.github.com>
2022-01-10 00:11:56 +0200

    Initial commit

---
+++ project/[2021-2022] TINY_Compiler_Template - For Students/JASON_Compiler/Token_Class.cs
@@ -0,0 +1,8 @@
+﻿public enum Token_Class
+{
+    Begin, Call, Declare, End, Do, Else, EndIf, EndUntil, EndWhile, If, Data_Type(INT),
+    Parameters, Procedure, Program, Read, Real, Set, Then, Until, While, Write,
+    Dot, Semicolon, Comma, LParanthesis, RParanthesis, EqualOp, EqualConOp, LessThanOp,
+    GreaterThanOp, NotEqualOp, PlusOp, MinusOp, MultiplyOp, DivideOp,
+    Identifier, Number, Left_CurlyBracket, Float, String, main, Repeat, Else_If, EndL, Return,AndOperator,OrOperator, Comment, Right_CurlyBracket
+}
